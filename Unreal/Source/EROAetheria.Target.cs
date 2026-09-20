fc1e03cb92abcfd92f6b440fe9f9d492ba67adb4
razorback3110-coder <razorback3110@gmail.com>
2026-09-20 20:03:15 +0200

    feat: add Unreal game and dedicated server targets

---
+++ Unreal/Source/EROAetheria.Target.cs
@@ -0,0 +1,13 @@
+using UnrealBuildTool;
+using System.Collections.Generic;
+
+public class EROAetheriaTarget : TargetRules
+{
+    public EROAetheriaTarget(TargetInfo Target) : base(Target)
+    {
+        Type = TargetType.Game;
+        DefaultBuildSettings = BuildSettingsVersion.V6;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
+        ExtraModuleNames.Add("EROAetheria");
+    }
+}
