a0da9bf7b10409a86af6a3d468f30c5b2d1d4c27
razorback3110-coder <razorback3110@gmail.com>
2026-09-20 20:52:17 +0200

    fix: use UE 5.8.2 default target build settings

--- Unreal/Source/EROAetheriaEditor.Target.cs
+++ Unreal/Source/EROAetheriaEditor.Target.cs
@@ -5,11 +5,8 @@ public class EROAetheriaEditorTarget : TargetRules
     public EROAetheriaEditorTarget(TargetInfo Target) : base(Target)
     {
         Type = TargetType.Editor;
-        DefaultBuildSettings = BuildSettingsVersion.V6;
+        DefaultBuildSettings = BuildSettingsVersion.V7;
         IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_8;
-        UnreachableWarningLevel = WarningLevel.Error;
-        ReturnTypeWarningLevel = WarningLevel.Error;
-        DanglingElseWarningLevel = WarningLevel.Error;
         ExtraModuleNames.Add("EROAetheria");
     }
 }
