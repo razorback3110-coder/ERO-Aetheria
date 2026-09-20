e2fe14a7d1dbf7854b91992c38895160fa926ac7
razorback3110-coder <razorback3110@gmail.com>
2026-09-20 20:02:13 +0200

    feat: bootstrap Unreal Engine MMORPG migration

---
+++ Unreal/Source/EROAetheria/EROAetheria.Build.cs
@@ -0,0 +1,24 @@
+using UnrealBuildTool;
+
+public class EROAetheria : ModuleRules
+{
+    public EROAetheria(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "GameplayAbilities",
+            "GameplayTags",
+            "GameplayTasks",
+            "NetCore",
+            "MassEntity",
+            "MassGameplay"
+        });
+    }
+}
