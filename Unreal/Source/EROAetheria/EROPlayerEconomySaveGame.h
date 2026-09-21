25c3f207978c2028038cb625092caf8ff2df72c3
razorback3110-coder <razorback3110@gmail.com>
2026-09-21 13:47:59 +0200

    feat: persist character progression with economy

--- Unreal/Source/EROAetheria/EROPlayerEconomySaveGame.h
+++ Unreal/Source/EROAetheria/EROPlayerEconomySaveGame.h
@@ -3,6 +3,8 @@
 #include "CoreMinimal.h"
 #include "GameFramework/SaveGame.h"
 #include "EROPlayerEconomyState.h"
+#include "EROClassTypes.h"
+#include "EROCharacterAppearanceTypes.h"
 #include "EROPlayerEconomySaveGame.generated.h"
 
 UCLASS()
@@ -12,11 +14,26 @@ class EROAETHERIA_API UEROPlayerEconomySaveGame final : public USaveGame
 
 public:
     UPROPERTY()
-    int32 SchemaVersion = 1;
+    int32 SchemaVersion = 2;
 
     UPROPERTY()
     int64 GoldBalance = 0;
 
     UPROPERTY()
     TArray<FEROInventoryStack> Inventory;
+
+    UPROPERTY()
+    int32 CharacterLevel = 1;
+
+    UPROPERTY()
+    int64 CharacterExperience = 0;
+
+    UPROPERTY()
+    EEROPlayerClass CharacterClass = EEROPlayerClass::Warrior;
+
+    UPROPERTY()
+    FName EquippedWeaponId = NAME_None;
+
+    UPROPERTY()
+    EEROWeaponFamily EquippedWeaponFamily = EEROWeaponFamily::Sword;
 };
