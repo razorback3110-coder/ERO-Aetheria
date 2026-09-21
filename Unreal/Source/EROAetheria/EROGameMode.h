830c389e2157e0baac07315bd7f7b72e4ae62c3f
razorback3110-coder <razorback3110@gmail.com>
2026-09-21 05:50:09 +0200

    feat: make Unreal starter encounters data-driven

--- Unreal/Source/EROAetheria/EROGameMode.h
+++ Unreal/Source/EROAetheria/EROGameMode.h
@@ -2,6 +2,7 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/GameModeBase.h"
+#include "EROEncounterDirectorTypes.h"
 #include "EROGameMode.generated.h"
 
 UCLASS()
@@ -12,6 +13,12 @@ class EROAETHERIA_API AEROGameMode final : public AGameModeBase
 public:
     AEROGameMode();
 
+    UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="ERO|World|Encounters")
+    TArray<FEROEncounterSpawnDefinition> StarterEncounters;
+
 protected:
     virtual void BeginPlay() override;
+
+private:
+    void SpawnConfiguredEncounters();
 };
