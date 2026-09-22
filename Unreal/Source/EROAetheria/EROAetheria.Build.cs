96116f9c1c483c7abb8f0a6432e2c2ce9d4fdb6f
razorback3110-coder <razorback3110@gmail.com>
2026-09-22 02:10:59 +0200

    fix: add Niagara module dependency

--- Unreal/Source/EROAetheria/EROAetheria.Build.cs
+++ Unreal/Source/EROAetheria/EROAetheria.Build.cs
@@ -17,7 +17,8 @@ public class EROAetheria : ModuleRules
             "GameplayTags",
             "GameplayTasks",
             "NetCore",
-            "MassEntity"
+            "MassEntity",
+            "Niagara"
         });
     }
 }
